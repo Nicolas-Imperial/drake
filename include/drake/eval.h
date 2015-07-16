3fad2e48e42848029a500fe8797ba3c813a76485
Nicolas Melot <nicolas.melot@liu.se>
2015-07-16 16:31:56 -0700

    * Added missing header

---
+++ include/drake/eval.h
@@ -0,0 +1,8 @@
+#ifndef DRAKE_EVAL
+#define DRAKE_EVAL
+
+// pthread in icc 8.1/gcc 3.4.0 *hate* when a pointer is named "kill"
+// Forbidden: kill
+drake_time_t *init, *start, *work, *killed;
+
+#endif
