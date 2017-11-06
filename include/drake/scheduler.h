7a608bbbc79a24d05ca8936af8a011ecbf69af49
Nicolas Melot <n.melot@imperial.ac.uk>
2017-11-06 11:51:56 +0000

    * Added missing header

---
+++ include/drake/scheduler.h
@@ -0,0 +1,28 @@
+/*
+ Copyright 2015 Nicolas Melot
+
+ This file is part of Drake.
+
+ Drake is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ Drake is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with Drake. If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+#include <drake/stream.h>
+
+#ifndef DRAKE_SCHEDULER_H_
+#define DRAKE_SCHEDULER_H_ 1
+
+int drake_dynamic_scheduler(drake_stream_t* stream);
+
+#endif
